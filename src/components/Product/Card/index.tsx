44b58b41ea98d68d7edc5aa78e0709e03797d7e6
ayrton <ayrtoncoelhods@gmail.com>
2024-12-01 21:55:05 -0300

    product card

--- src/components/Product/Card/index.tsx
+++ src/components/Product/Card/index.tsx
@@ -6,6 +6,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Product } from '../../../types/product';
 import { Link } from 'react-router-dom';
+import { Box, Tooltip } from '@mui/material';
 
 export default function ProductCard({ product }: { product: Product }) {
   return (
@@ -17,18 +18,22 @@ export default function ProductCard({ product }: { product: Product }) {
           title="product"
         />
       </Link>
-      <CardContent sx={{ height: 120}}>
-        <Link to={`/products/${product._id}`} style={{ textDecoration: 'none' }}>
-          <Typography gutterBottom variant="h6" component="div">
-            {product?.name}
+      <CardContent sx={{ height: 250}}>
+        <Box sx={{ display: "flex", justifyContent: "space-between"}}>
+          <Link to={`/products/${product._id}`} style={{ textDecoration: 'none' }}>
+            <Typography gutterBottom variant="h6" component="div">
+              {product?.name}
+            </Typography>
+          </Link>
+          <Typography variant='h6' component="div">
+            $ {product?.price}
           </Typography>
-        </Link>
-        <Typography variant='body1' component="div">
-          $ {product?.price}
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          {product?.description}
-        </Typography>
+        </Box>
+        <Tooltip title={product?.description}>
+          <Typography variant="body2" sx={{ color: 'text.secondary', marginTop: 2 }}>
+            {product?.description && product.description.length > 50 ? product.description.slice(0, 50) + '...' : product.description}
+          </Typography>
+        </Tooltip>
       </CardContent>
       <CardActions>
         <Button size="small">Share</Button>
